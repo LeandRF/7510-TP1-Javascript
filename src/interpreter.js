ecf6d5a21c80369f1893a3c19cd9769816c10772
LeandRF <leandro.sosa@cencosud.com.ar>
2017-10-08 20:01:20 -0300

    Test git push commit

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,13 +1,47 @@
-var Interpreter = function () {
+var Fact = function(name, args) {
+     this.name = name;
+     this.args = [args];
+};
+ 
 
-    this.parseDB = function (params, paramss, paramsss) {
-
-    }
-
-    this.checkQuery = function (params) {
+var facts = [];
+var rules =[];
+	
+this.checkQuery = function (params) {
+	 
         return true;
     }
 
+this.parseDB =  function(db){
+	//CheckDB else => false
+
+	db.map(function(line){(CheckRule(line) ? AddRule(line): AddFact(line))});	
+};
+
+function CheckRule(line){	
+    line.includes(":-");
+};
+
+function AddRule(line)
+{
+	//TODO
+};
+
+function ParseFact(line){
+	var ini = line.indexOf("(");  		
+	var last = line.indexOf(")");   	
+	return [line.substring(0,ini), line.substring(ini+1,last)];
 }
+function AddFact(line){
+	var fact = ParseFact(line);		
+	for (i = 0; i < facts.length; i++) { 			
+    		if(facts[i].name == fact[0]) { facts[i].args.push(fact[1])					
+					return;
+				}
+		}
+	facts.push(new Fact(fact[0],fact[1]));
+};
+
+
 
 module.exports = Interpreter;
